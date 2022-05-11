57efd8bd28f7a153be4c485a8baab1986b005a0e
richboy77777 <goodworld77777@gmail.com>
2022-05-11 16:02:49 +0900

    5월 11일

--- src/main/java/com/helpme/travel/module/admin/AdminDao.java
+++ src/main/java/com/helpme/travel/module/admin/AdminDao.java
@@ -19,11 +19,11 @@ public class AdminDao {
    private static String namespace = "com.helpme.travel.module.admin.AdminMpp";
    
    public List<Admin> selectHost(){return sqlSession.selectList(namespace + ".selectHost", "");}
-<<<<<<< HEAD
+
    public Admin selectOneHost(AdminVo vo){return sqlSession.selectOne(namespace + ".selectOneHost", vo);}
-=======
+
    public Admin selectOneMember(AdminVo vo){return sqlSession.selectOne(namespace + ".selectOneMember", vo);}
->>>>>>> branch 'main' of https://github.com/Sjrnfl23/travel_site.git
+
    
    public List<Admin> selectMember(){return sqlSession.selectList(namespace + ".selectMember", "");}
    
