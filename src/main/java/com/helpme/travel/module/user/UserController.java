69c62e0a3f50fcaf1c256b1a75add208527025c4
ina <naleeyoun@naver.com>
2022-06-21 11:54:24 +0900

    t

--- src/main/java/com/helpme/travel/module/user/UserController.java
+++ src/main/java/com/helpme/travel/module/user/UserController.java
@@ -173,9 +173,8 @@ public class UserController {
 	public String UserLodgingView(@ModelAttribute("vo")UserVo vo, User dto, Model model) throws Exception {
 		
 		
-		/*
-		 * Double avg=service.selectOneReviewAvg(vo); model.addAttribute("avg",avg);
-		 */
+		Double avg=service.selectOneReviewAvg(vo);
+		model.addAttribute("avg",avg);
 		int count = service.selectReviewCount(vo);
 		vo.setParamsPaging(count);
 		 if(count != 0) {										
